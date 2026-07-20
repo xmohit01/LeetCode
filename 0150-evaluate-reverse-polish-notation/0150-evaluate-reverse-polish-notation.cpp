3a262837980d48a9f4f4813e4a4e967f16266f0a
Mohit Gupta <gupta437mohit@gmail.com>
2026-07-20 16:40:51 +0530

    Time: 0 ms (100%), Space: 17 MB (88.13%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -4,10 +4,9 @@ public:
         stack<int> st;
 
         for(auto& token : tokens){
-            string temp = token;
 
-            if(temp != "+" && temp != "-" && temp != "/" && temp != "*"){
-                st.push(stoi(temp));
+            if(token != "+" && token != "-" && token != "/" && token != "*"){
+                st.push(stoi(token));
                 continue;
             }
 
@@ -15,9 +14,9 @@ public:
             int first = st.top(); st.pop();
 
             int opr;
-            if(temp == "+") opr = first + second;
-            else if(temp == "-") opr = first - second;
-            else if(temp == "/") opr = first / second;
+            if(token == "+") opr = first + second;
+            else if(token == "-") opr = first - second;
+            else if(token == "/") opr = first / second;
             else opr = first * second;
 
             st.push(opr);
