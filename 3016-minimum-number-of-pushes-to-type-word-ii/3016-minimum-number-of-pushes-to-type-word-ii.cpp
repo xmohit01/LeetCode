e19a861e4dd77117b0daca08dbc054f48f56227f
Mohit Gupta <gupta437mohit@gmail.com>
2026-07-31 08:44:27 +0530

    Time: 8 ms (61.48%), Space: 28.6 MB (15.83%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,27 +5,24 @@ public:
         for(char c : word){
             freq[c - 'a']++;
         }
-
-        int distinct = 0;
         
         sort(freq.rbegin(), freq.rend());
 
         int ans = 0;
         for(int i = 0; i < 26; i++){
-            if(freq[i] > 0){
-                distinct++;
-                if(distinct <= 8){
-                    ans += freq[i];
-                }
-                else if(distinct <= 16){
-                    ans += freq[i] * 2;
-                }
-                else if(distinct <= 24){
-                    ans += freq[i] * 3;
-                }
-                else{
-                    ans += freq[i] * 4;
-                }
+            if(freq[i] == 0) break;
+
+            if(i + 1 <= 8){
+                ans += freq[i];
+            }
+            else if(i + 1 <= 16){
+                ans += freq[i] * 2;
+            }
+            else if(i + 1 <= 24){
+                ans += freq[i] * 3;
+            }
+            else{
+                ans += freq[i] * 4;
             }
         }
         return ans;
