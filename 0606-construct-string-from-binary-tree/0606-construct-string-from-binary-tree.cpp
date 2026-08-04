19cb698704400fce08bf4f3f3ba227fe9447a373
Mohit Gupta <gupta437mohit@gmail.com>
2026-08-04 22:27:26 +0530

    Time: 0 ms (100%), Space: 25.5 MB (64.91%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -12,24 +12,24 @@
 class Solution {
 public:
     void builder(TreeNode* root, string& str){
-        if(root == NULL){
+        if(root == NULL){   // if null ofcourse we have to return
             return;
         }
 
-        if(root->left == NULL && root->right == NULL){
+        if(root->left == NULL && root->right == NULL){   // if both are NULL then we just have to add value bcoz the brackets are already taken care at our calling area!!!
             str += to_string(root->val);
             return;
         }
 
-        str += to_string(root->val);
+        str += to_string(root->val);  // adding the current value
 
-        if(root->left != NULL || root->right != NULL){
+        if(root->left != NULL || root->right != NULL){    // if left exists then ofcourse we have to add than value in brackets and if left does'nt exist but right exist then we should add empty brackets for that we will call it!!
             str += '(';
             builder(root->left, str);
             str += ")";
         }
 
-        if(root->right != NULL){
+        if(root->right != NULL){    // ofcourse rigth will add if it will exist..
             str += "(";
             builder(root->right, str);
             str += ')';
