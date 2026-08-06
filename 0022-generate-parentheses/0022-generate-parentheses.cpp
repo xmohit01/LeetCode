471809a013d4c572cdf29294657783ab4fd531e0
Mohit Gupta <gupta437mohit@gmail.com>
2026-08-06 11:52:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (95.84%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void helper(int n, string& temp, vector<string>& dp, vector<string>& ans, int openBracks, int closeBracks){
+    void helper(int n, string& temp, vector<string>& ans, int openBracks, int closeBracks){
         if(temp.length() == 2 * n){
             ans.push_back(temp);
             return;
@@ -9,23 +9,22 @@ public:
         if(openBracks < n){
             temp += '(';
 
-            helper(n, temp, dp, ans, openBracks + 1, closeBracks);
+            helper(n, temp, ans, openBracks + 1, closeBracks);
             temp.pop_back();
         }
         if(closeBracks < openBracks){
             temp += ')';
 
-            helper(n, temp, dp, ans, openBracks, closeBracks + 1);
+            helper(n, temp, ans, openBracks, closeBracks + 1);
             temp.pop_back();
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        vector<string> dp(n);
         vector<string> ans;
         string temp = "";
 
-        helper(n, temp, dp, ans, 0, 0);
+        helper(n, temp, ans, 0, 0);
         return ans;
     }
 };
