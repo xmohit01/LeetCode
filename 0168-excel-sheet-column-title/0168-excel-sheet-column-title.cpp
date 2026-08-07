8551a35488e358c2b4c22ec95191b8125847e748
Mohit Gupta <gupta437mohit@gmail.com>
2026-08-07 17:15:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (59.59%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans = "";
+
+        while(columnNumber != 0){
+            columnNumber--;
+            int currDigit = columnNumber % 26;
+            
+            ans.push_back(currDigit + 'A');
+
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
